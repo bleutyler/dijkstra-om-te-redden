55f644885d22964338a73b57f81e444769270ef6
Tyler Slijboom <tyler.slijboom@gmail.com>
2017-12-24 05:31:45 -0500

    getters and setters

--- traingraph.py
+++ traingraph.py
@@ -9,8 +9,8 @@ class traingraph:
 		self.edges_dictionary = new_edges
 
 	def calculate_shortest_route( self, first_node, second_node ):
-		if first_node in self.edges_dictionary and second_node in self.edges_dictionary[ first_node ]:
-			return self.edges_dictionary[ first_node ][ second_node ]
+		if self.exists( first_node, second_node ):
+			return self.get( first_node, second_node )
 		else:
 			# use dijkstra's on this until you get to the node we want
 			if first_node == second_node:
@@ -30,6 +30,26 @@ class traingraph:
 
 		return None
 
+	def put( self, node_1, node_2, value ):
+		if node_1 in self.edges_dictionary:
+			pass
+		else:
+			self.edges_dictionary[ node_1 ] = {}
+		self.edges_dictionary[ node_1 ][ node_2 ] = value
+	
+	def exists( self, node_1, node_2 ):
+		if node_1 in self.edges_dictionary:
+			if node_2 in self.edges_dicionary[ node_1 ]:
+				return True
+
+		return False
+		
+
+	def get( self, node_1, node_2 ):
+		if self.exists( node_1, node_2 ):
+			return self.edges_dictionary[ node_1 ][ node_2 ]
+		else:
+			return None
 
 	def return_sub_graph_with_node_removed( self, node_to_delete ):
 		# iterate through unique edges
@@ -38,4 +58,11 @@ class traingraph:
 	def return_list_of_unique_nodes( self ):
 		pass
 
-	def calculate_number_of_stops_between( self, first_node, second_node ):
+	#def calculate_number_of_stops_between( self, first_node, second_node ):
+	#	pass
+
+	def calculate_all_routes( self, first_node, second_node ):
+		pass
+
+	def get_neighbours_of_a_node( self, node ):
+		pass
